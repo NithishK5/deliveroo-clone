2c499d137c92ae9033db644afd0d4fd7551c6fa0
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2023-01-18 16:05:57 +0000

    components created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -8,7 +15,7 @@ import {
   MagnifyingGlassIcon,
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
-
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -21,48 +28,70 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-        {/*Header*/}
-        <View className="flex-row pb-3 mx-4 space-x-2 px-4">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      {/*Header*/}
+      <View className="flex-row pb-3 mx-4 space-x-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/*Search*/}
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurant and cuisines"
+            keyboardType="default"
           />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
         </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
 
-        {/*Search*/}
+      {/*Body*/}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* Categories */}
 
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <MagnifyingGlassIcon color="gray" size={20} />
-            <TextInput
-              placeholder="Restaurant and cuisines"
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsVerticalIcon color="#00CCBB" />
-        </View>
+        <Categories />
 
-        {/*Body*/}
-        <ScrollView className="bg-gray-100"
-            contentContainerStyle={{
-                paddingBottom: 100,
-            }}
-        >
-            {/* Categories */}
-            <Categories/>
-            {/* Featured Rows */}
-        </ScrollView>
-      
+        {/* Featured Rows */}
+
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+
+        {/* Tasty Discounts */}
+        <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+
+        {/* Offers near you */}
+        <FeaturedRow
+          id="12345"
+          title="Offers near you"
+          description="Why not support your local restaurant tonight!"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
